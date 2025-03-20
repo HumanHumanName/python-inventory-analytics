752900329d2d36ee9f49dc346cb5004b2c39bafa
HumanHumanName <k12132486@kotharischool.edu.in>
2025-03-20 22:54:54 +0530

    Addded a line to create database 'test_database'

--- database_handler.py
+++ database_handler.py
@@ -5,3 +5,10 @@ conn_obj = conn.connect(host = "localhost", user = "kores",passwd = '0.314159')
 
 if conn_obj.is_connected() == True:
     print("Connected successfully")
+
+cursor = conn_obj.cursor()
+
+#working under a database called test_database
+#cursor.execute("CREATE DATABASE test_database")
+
+
