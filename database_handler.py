11d357ce4a98e3f70bb21f36c5c292d93dbaceca
HumanHumanName <k12132486@kotharischool.edu.in>
2025-03-21 17:11:46 +0530

    Added a table in database and generalied code for other systems

--- database_handler.py
+++ database_handler.py
@@ -1,14 +1,26 @@
 # Requires MqSQL connector
 import mysql.connector as conn
 
-conn_obj = conn.connect(host = "localhost", user = "kores",passwd = '0.314159')
+user_name = str(input("Please enter your MySQL username: "))
+user_password = str(input("Please enter your MySQL password: "))
+print("Running locally...")
+
+conn_obj = conn.connect(host = "localhost", user = user_name, passwd = user_password)
 
 if conn_obj.is_connected() == True:
     print("Connected successfully")
 
 cursor = conn_obj.cursor()
 
-#working under a database called test_database
-#cursor.execute("CREATE DATABASE test_database")
+# Working under a database called test_database; Setup
+# Replace test_database with real name later
 
+#cursor.execute("CREATE DATABASE IF NOT EXISTS test_database")
+
+# cursor.execute("""CREATE TABLE test_database.inventory
+#                 (
+#                     item_id int(10) not null,
+#                     item_name varchar(20) not null,
+#                     item_count int(10) not null
+#                 );""")
 
