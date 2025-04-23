fe6eb5a64c794f78d68ad14e0c02d89c1dafa6ae
Human (human-name) <k12132486@kotharischool.edu.in>
2025-04-23 10:21:55 +0530

    Updated visual handler

--- visual_handler.py
+++ visual_handler.py
@@ -3,5 +3,10 @@ import tkinter as tk
 def run_GUI():
   root = tk.Tk()
   root.title("Test view")
+  root.geometry("500x300")
+  frame = tk.Frame(root)
+  frame.grid(row=10,column = 10)
+  tk.Label(frame, text='Hello World!').grid(row = 0, column = 0)
+  tk.Label(frame, text='Hello World!').grid(row = 1, column = 1)
 
   root.mainloop()
