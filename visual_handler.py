064c4d40886023401cd3a02cebeb4bfd010903a7
HumanHumanName <k12132486@kotharischool.edu.in>
2025-04-27 11:21:52 +0530

    Trimmed database viewer and added scrollbar

--- visual_handler.py
+++ visual_handler.py
@@ -5,9 +5,9 @@ import database_handler
 def run_GUI():
   root = tk.Tk()
   root.title("Test view")
-  root.geometry("690x400")
+  root.geometry("600x385")
 
-  # Setting style
+  # setting style
   style=ttk.Style()
   style.theme_use('clam')
 
@@ -15,14 +15,28 @@ def run_GUI():
   database_label = tk.Label(root,text = "Database Viewer", relief = "raised")
   database_label.grid(row = 0,
                      column = 0,
-                     padx = 100,
-                     pady = 10,
+                     padx = 90,
+                     pady = 10
                      )
 
-  inventory_data = database_handler.retrieve_via_sql_query("*","inventory")
+  inventory_data = database_handler.retrieve_via_sql_query("item_id,item_name,item_cost,item_final_cost,item_stock","inventory")
 
-  database_viewer = ttk.Treeview(root, columns = ("item_id","item_name","item_cost","item_gst","item_discount","item_final_cost","item_margin","item_stock","item_manufacturer_name","item_manufacturer_incharge","item_manufacturer_contact_no"), show = 'headings')
-  # CC TRIM COLUMS!!!! and put in notebook
+  database_viewer = ttk.Treeview(root, columns = ("item_id","item_name","item_cost","item_final_cost","item_stock"), show = 'headings', height = 15)
+  database_viewer.grid(row = 1,
+                       column = 0,
+                       padx = 5,
+                       sticky = "e"
+                       )
+
+  # creates the scrollbar
+  scrollbar = ttk.Scrollbar(root, orient = "vertical", command = database_viewer.yview)
+  scrollbar.grid(row = 1,
+                 column = 1,
+                 sticky="nswe"
+                 )
+  database_viewer.configure(yscrollcommand=scrollbar.set)
+
+  # CC put in notebook
   # initialise columns
   database_viewer.column("item_id", anchor="center", width=40)
   database_viewer.heading('item_id', text = 'S.No')
@@ -33,29 +47,11 @@ def run_GUI():
   database_viewer.column("item_cost", anchor="center", width=55)
   database_viewer.heading('item_cost', text = 'Cost')
 
-  database_viewer.column("item_gst", anchor="center", width=45)
-  database_viewer.heading('item_gst', text = 'GST')
-
-  database_viewer.column("item_discount", anchor="center", width=40)
-  database_viewer.heading('item_discount', text = '%off')
-
   database_viewer.column("item_final_cost", anchor="center", width=45)
-  database_viewer.heading('item_final_cost', text = 'total')
-
-  database_viewer.column("item_margin", anchor="center", width=60)
-  database_viewer.heading('item_margin', text = 'margin')
+  database_viewer.heading('item_final_cost', text = 'Total')
 
   database_viewer.column("item_stock", anchor="center", width=50)
-  database_viewer.heading('item_stock', text = 'stock')
-
-  database_viewer.column("item_manufacturer_name", anchor="center", width=85)
-  database_viewer.heading('item_manufacturer_name', text = 'M. Name')
-
-  database_viewer.column("item_manufacturer_incharge", anchor="center", width=90)
-  database_viewer.heading('item_manufacturer_incharge', text = 'M. Incharge')
-
-  database_viewer.column("item_manufacturer_contact_no", anchor="center", width=90)
-  database_viewer.heading('item_manufacturer_contact_no', text = 'M. Contact')
+  database_viewer.heading('item_stock', text = 'Stock')
 
   database_viewer.grid(row = 1,column = 0)
 
