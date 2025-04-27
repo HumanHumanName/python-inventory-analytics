536554e18a345532a9f64f2b306fb9e7a80d9d6c
HumanHumanName <k12132486@kotharischool.edu.in>
2025-04-27 16:17:13 +0530

    Added orders viewer to homepage in a notebook

--- visual_handler.py
+++ visual_handler.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import ttk # CC make treeview table via this
+from tkinter import ttk
 import database_handler
 
 def run_GUI():
@@ -11,7 +11,6 @@ def run_GUI():
   style=ttk.Style()
   style.theme_use('clam')
 
-  # database viewer
   database_label = tk.Label(root,text = "Database Viewer", relief = "raised")
   database_label.grid(row = 0,
                      column = 0,
@@ -19,44 +18,104 @@ def run_GUI():
                      pady = 10
                      )
 
+  # Notebook to contain the two tables in our database
+  tables_notebook = ttk.Notebook(root)
+  tables_notebook.grid(row = 1,
+                       column = 0,
+                       padx = 10
+                       )
+
+  inventory_tab= ttk.Frame(tables_notebook)
+  tables_notebook.add(inventory_tab, text= "Inventory")
+
+  orders_tab= ttk.Frame(tables_notebook)
+  tables_notebook.add(orders_tab, text= "Orders")
+
+  # inventory viewer code
   inventory_data = database_handler.retrieve_via_sql_query("item_id,item_name,item_cost,item_final_cost,item_stock","inventory")
 
-  database_viewer = ttk.Treeview(root, columns = ("item_id","item_name","item_cost","item_final_cost","item_stock"), show = 'headings', height = 15)
-  database_viewer.grid(row = 1,
+  inventory_viewer = ttk.Treeview(inventory_tab,
+                                  columns = ("item_id","item_name","item_cost","item_final_cost","item_stock"),
+                                  show = 'headings',
+                                  height = 13
+                                  )
+  inventory_viewer.grid(row = 1,
                        column = 0,
-                       padx = 5,
                        sticky = "e"
                        )
 
-  # creates the scrollbar
-  scrollbar = ttk.Scrollbar(root, orient = "vertical", command = database_viewer.yview)
+  # creating the scrollbar
+  scrollbar = ttk.Scrollbar(inventory_tab, orient = "vertical", command = inventory_viewer.yview)
   scrollbar.grid(row = 1,
                  column = 1,
                  sticky="nswe"
                  )
-  database_viewer.configure(yscrollcommand=scrollbar.set)
+  inventory_viewer.configure(yscrollcommand=scrollbar.set)
 
-  # CC put in notebook
-  # initialise columns
-  database_viewer.column("item_id", anchor="center", width=40)
-  database_viewer.heading('item_id', text = 'S.No')
+  # initialising columns
+  inventory_viewer.column("item_id", anchor="center", width=40)
+  inventory_viewer.heading('item_id', text = 'S.No')
 
-  database_viewer.column("item_name", anchor="center", width=85)
-  database_viewer.heading('item_name', text = 'Name')
+  inventory_viewer.column("item_name", anchor="center", width=85)
+  inventory_viewer.heading('item_name', text = 'Name')
 
-  database_viewer.column("item_cost", anchor="center", width=55)
-  database_viewer.heading('item_cost', text = 'Cost')
+  inventory_viewer.column("item_cost", anchor="center", width=55)
+  inventory_viewer.heading('item_cost', text = 'Cost')
 
-  database_viewer.column("item_final_cost", anchor="center", width=45)
-  database_viewer.heading('item_final_cost', text = 'Total')
+  inventory_viewer.column("item_final_cost", anchor="center", width=45)
+  inventory_viewer.heading('item_final_cost', text = 'Total')
 
-  database_viewer.column("item_stock", anchor="center", width=50)
-  database_viewer.heading('item_stock', text = 'Stock')
+  inventory_viewer.column("item_stock", anchor="center", width=50)
+  inventory_viewer.heading('item_stock', text = 'Stock')
 
-  database_viewer.grid(row = 1,column = 0)
+  inventory_viewer.grid(row = 1,column = 0)
 
-  # insert values into a database_viewer
+  # insert values into inventory_viewer
   for i in inventory_data:
-    database_viewer.insert(parent = '', index = tk.END, values = i)
+    inventory_viewer.insert(parent = '', index = tk.END, values = i)
+
+  # orders viewer code
+  orders_data = database_handler.retrieve_via_sql_query("order_id,order_item_name,order_customer_name,order_final_cost,order_quantity","orders")
+
+  orders_viewer = ttk.Treeview(orders_tab,
+                              columns = ("order_id","order_item_name","order_customer_name","order_final_cost","order_quantity"),
+                              show = 'headings',
+                              height = 13
+                              )
+  orders_viewer.grid(row = 1,
+                       column = 0,
+                       sticky = "e"
+                       )
+
+  # creating the scrollbar
+  scrollbar = ttk.Scrollbar(orders_tab, orient = "vertical", command = orders_viewer.yview)
+  scrollbar.grid(row = 1,
+                 column = 1,
+                 sticky="nswe"
+                 )
+  orders_viewer.configure(yscrollcommand=scrollbar.set)
+
+  # initialising columns
+  orders_viewer.column("order_id", anchor="center", width=40)
+  orders_viewer.heading('order_id', text = 'S.No')
+
+  orders_viewer.column("order_item_name", anchor="center", width=80)
+  orders_viewer.heading('order_item_name', text = 'Item')
+
+  orders_viewer.column("order_customer_name", anchor="center", width=60)
+  orders_viewer.heading('order_customer_name', text = 'Name')
+
+  orders_viewer.column("order_final_cost", anchor="center", width=55)
+  orders_viewer.heading('order_final_cost', text = 'Total')
+
+  orders_viewer.column("order_quantity", anchor="center", width=40)
+  orders_viewer.heading('order_quantity', text = 'Amt')
+
+  orders_viewer.grid(row = 1,column = 0)
+
+  # insert values into orders_viewer
+  for i in orders_data:
+    orders_viewer.insert(parent = '', index = tk.END, values = i)
+
 
   root.mainloop()
